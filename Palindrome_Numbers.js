8afd04095b68742e1446ed6f018f5ee24b0c8647
AminesCodes <aminebensalem@pursuit.org>
2019-07-18 12:15:55 -0400

    Palindrome Numbers Code

--- Palindrome_Numbers.js
+++ Palindrome_Numbers.js
@@ -5,8 +5,20 @@ An integer is a palindrome when it reads the same backward as forward.
  * @param {number} x
  * @return {boolean}
  */
-var isPalindrome = function(x) {
-    let str = x.toString();
+ const isPalindrome = (num) => {
+     let str = num.toString();
+     let reverseStr = '';
 
+     for (let i = str.length - 1; i >= 0; i--){
+       reverseStr += str[i];
+     }
 
-};
+     if (str === reverseStr){
+       return true;
+     } else {
+       return false;
+     }
+ };
+
+let num = 1221;
+console.log(isPalindrome(num));
